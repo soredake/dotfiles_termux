b9d1c2cf02165b62494d52fed527f5097b81bc79
name <email@example>
2016-06-23 13:32:54 +0300

    Daily update

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,6 +8,7 @@ docker run -dt \
        -v "$HOME/mdata/syncthing:/home/user/Sync" \
        rpi2-syncthing syncthing -no-browser -home="/home/user/syncthing" &&
 docker run -dt --name dnscrypt --restart always -p 53:53 rpi2-dnscrypt-proxy &&
+
 # https://github.com/derand/rpi_i2pd
 docker run -dt -p 7070:7070 -p 4446:4446 -p 7650:7650 -v $HOME/mdata/busybox-armv7l:/bin/busybox --name i2pd i2pd &&
 docker run -dt --name baikal -p 80:80 -p 443:443 -v $HOME/cdata/baikal:/var/www/Specific ckulka/rpi-baikal
