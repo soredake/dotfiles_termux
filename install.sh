c100f6566f118f0faa88961d78a8dd0ca4b936b8
soredake <5204968+soredake@users.noreply.github.com>
2025-05-08 10:03:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/system/bin/env bash
 
 pkg upgrade -y
-pkg install -y git stow fish
+pkg install -y git stow fish vitetris
 git clone https://github.com/soredake/dotfiles_termux.git "$HOME/git/dotfiles_termux"
 "$HOME/git/dotfiles_termux/scripts/home.sh"
 fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
