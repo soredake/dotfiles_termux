5cfa1510486f68d4971f332ffd033f439fb200bc
soredake <5204968+soredake@users.noreply.github.com>
2026-01-07 14:19:32 +0200

    Update install script to use bash and new fisher URL

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 pkg upgrade -y
 pkg install -y git stow fish vitetris android-tools
 git clone https://github.com/soredake/dotfiles_termux.git "$HOME/git/dotfiles_termux"
-"$HOME/git/dotfiles_termux/scripts/home.sh"
-fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
+bash "$HOME/git/dotfiles_termux/scripts/home.sh"
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; fisher install jorgebucaran/fisher pure-fish/pure"
 chsh -s fish
