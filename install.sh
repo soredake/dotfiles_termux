2cafad0897fd38866d34453e40dacdcba778beb6
soredake <5204968+soredake@users.noreply.github.com>
2026-01-07 14:26:24 +0200

    Disable container detection in install script
    Disable container detection for pure prompt.

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ pkg install -y git stow fish vitetris android-tools
 git clone https://github.com/soredake/dotfiles_termux.git "$HOME/git/dotfiles_termux"
 bash "$HOME/git/dotfiles_termux/scripts/home.sh"
 fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; fisher install jorgebucaran/fisher pure-fish/pure"
+# https://github.com/pure-fish/pure/issues/295#issuecomment-1673234460
+set --universal pure_enable_container_detection false
 chsh -s fish
